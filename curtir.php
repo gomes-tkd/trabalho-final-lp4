485927cdf6ce21f701a9fb3c7bac00234e7e4946
gomes-tkd <jgomestkd@gmail.com>
2023-07-01 17:01:09 -0300

    PHP - Adicionando redirecionamento após seguir e curtidas

--- curtir.php
+++ curtir.php
@@ -2,11 +2,13 @@
     require_once ("./config/conn.php");
     require_once ("./config/url.php");
     require_once ("./dao/PostagemDAO.php");
+    require_once ("./models/Mensagem.php");
 
     $postagemDao = new PostagemDAO($conn, $BASE_URL);
-
+    $mensagem = new Mensagem($BASE_URL);
     $id = $_GET["id"];
 
     $postagem = $postagemDao->getPostagem($id);
     $postagemDao->likePostagem($id);
+    $mensagem->setMensagem("Momento curtido com sucesso!", "succes", "postagem.php?id=$id");
 ?>
--- seguir_usuario.php
+++ seguir_usuario.php
@@ -2,11 +2,15 @@
     require_once ("./config/conn.php");
     require_once ("./config/url.php");
     require_once ("./dao/UsuarioDAO.php");
+    require_once ("./models/Mensagem.php");
 
     $id = $_GET["id"];
     $usuarioDao = new UsuarioDAO($conn, $BASE_URL);
     $usuario = $usuarioDao->findById($id);
+    $mensagem = new Mensagem($BASE_URL);
 
     $usuarioDao->followUsuario($usuario->id);
+
+    $mensagem->setMensagem("Momento curtido com sucesso!", "succes", "perfil_autor.php?id=$id");
 
 ?>
